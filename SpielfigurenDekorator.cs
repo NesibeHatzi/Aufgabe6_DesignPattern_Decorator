5bbf5fe5b64c7418a5c703dfa7d3d6b968d222ae
NesibeHatzi <37589455+NesibeHatzi@users.noreply.github.com>
2018-06-04 00:59:28 +0200

    Add files via upload

---
+++ SpielfigurenDekorator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespaceDesPatternDecorator
+{
+
+    abstract class Dekorierer : Spielfigur
+    {
+        private Spielfigur _meineFigur;
+
+        public Dekorierer(Spielfigur meineFigur)
+        {
+            this._meineFigur = meineFigur;
+        }
+
+        public override void Drohe()
+        {
+            _meineFigur.Drohe();
+        }
+    }
+
+    class ErkaelteteFigur : Dekorierer
+    {
+        public ErkaelteteFigur(Spielfigur meineFigur)
+            : base(meineFigur)
+        { }
+
+        public override void Drohe()
+        {
+            Console.Write(" Hust! ");
+            base.Drohe();
+        }
+    }
+
+     class HeisereFigur :  Dekorierer
+    {
+        public HeisereFigur(Spielfigur meineFigur)
+            : base(meineFigur)
+        { }
+
+        public override void Drohe()
+        {
+            Console.Write(" Räusper... ");
+           base.Drohe();
+        }
+    }
+}
